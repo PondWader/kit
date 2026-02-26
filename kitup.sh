f1195a195d53f955ce3e43dfd93e9783c5a57994
PondWader <66561610+PondWader@users.noreply.github.com>
2026-02-26 13:08:20 +0000

    update kitup.sh

--- kitup.sh
+++ kitup.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
 GO_VERSION="1.26.0"
+BIN_PATH="/usr/local/bin/kit"
 
 set -e
 
 install_dir=/tmp/kitup-install-$(date +%s)
 mkdir -p "${install_dir}"
+# Register cleanup
+trap 'rm -rf /tmp/kitup-install-*' EXIT
 
 # Clone the repository to build
 git clone --depth 1 https://github.com/PondWader/kit "$install_dir/kit"
 
-if [[ $(uname -m) != "x86_64" ]]; then 
-    echo "Unsupported architecture: $(uname -m)"
-    exit 1
-fi
+case $(uname -m) in
+    x86_64) GOARCH="amd64" ;;
+    aarch64|arm64) GOARCH="arm64" ;;
+    armv6l) GOARCH="armv6l" ;;
+    i386|i686) GOARCH="386" ;;
+    *) echo "Unsupported architecture: $(uname -m)"; exit 1 ;;
+esac
 
-# Download Go 
-wget -O "$install_dir/go.tar.gz" https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz 
+# Download Go
+wget -O "$install_dir/go.tar.gz" "https://go.dev/dl/go${GO_VERSION}.linux-${GOARCH}.tar.gz"
 tar xf "$install_dir/go.tar.gz" -C "$install_dir"
 
 # Build the execute
 echo "Building..."
-"$install_dir/go/bin/go" build -C "$install_dir/kit" -o "$(pwd)/kit" -trimpath -ldflags "-s -w" ./cmd
-echo "Built!"
+"$install_dir/go/bin/go" build -C "$install_dir/kit" -o "$BIN_PATH" -trimpath -ldflags "-s -w" ./cmd
+echo "Built and wrote kit to $BIN_PATH"
 
-rm -rf /tmp/kitup-install-*
